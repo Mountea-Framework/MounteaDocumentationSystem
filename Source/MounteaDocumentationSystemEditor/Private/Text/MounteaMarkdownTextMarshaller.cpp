b0450cab28038c6fdd41797f2e86163be00f0582
Dominik Morse <dominikpavlicek.business@gmail.com>
2025-02-19 22:07:19 +0100

    Testing Marshall

--- Source/MounteaDocumentationSystemEditor/Private/Text/MounteaMarkdownTextMarshaller.cpp
+++ Source/MounteaDocumentationSystemEditor/Private/Text/MounteaMarkdownTextMarshaller.cpp
@@ -20,53 +20,44 @@ void FMarkdownTextMarshaller::SetText(const FString& SourceString, FTextLayout& 
 
 	TArray<FTextRange> LineRanges;
 	FTextRange::CalculateLineRangesFromString(SourceString, LineRanges);
-
+	
 	const UMounteaDocumentationSystemEditorSettings* EditorSettings = GetDefault<UMounteaDocumentationSystemEditorSettings>();
 	const FSlateFontInfo DefaultFont = EditorSettings->GetEditorFont();
 
-	UE_LOG(LogTemp, Warning, TEXT("Applying font and color to %d lines"), LineRanges.Num());
-
-	for (int32 i = 0; i < LineRanges.Num(); i++)
+	for (const FTextRange& LineRange : LineRanges)
 	{
-		const FTextRange& LineRange = LineRanges[i];
-		FString LineText = SourceString.Mid(LineRange.BeginIndex, LineRange.Len());
-		TSharedRef<FString> SharedLineText = MakeShared<FString>(LineText);
+		TSharedRef<FString> LineText = MakeShared<FString>(SourceString.Mid(LineRange.BeginIndex, LineRange.Len()));
 		TArray<TSharedRef<IRun>> Runs;
-
-		// Determine the color
+		
+		FSlateFontInfo CustomFont = DefaultFont;
 		FLinearColor TextColor = FLinearColor::White;
-		const FLinearColor highlightColor = FLinearColor(FColor::FromHex("569cd6"));;
-		if (LineText.StartsWith("# ")) // Headings
-		{
-			TextColor = highlightColor;
-		}
-		else if (LineText.StartsWith("**") && LineText.EndsWith("**")) // Bold
-		{
-			TextColor =  highlightColor;
-		}
-		else if (LineText.StartsWith("_") && LineText.EndsWith("_")) // Italic
-		{
-			TextColor =  highlightColor;
-		}
-		else if (LineText.Contains("[") && LineText.Contains("](")) // Links
-		{
-			TextColor =  highlightColor;
-		}
+		const FLinearColor highlightedColor = FLinearColor(FColor::FromHex("569cd6"));
 
-		UE_LOG(LogTemp, Warning, TEXT("Line %d: '%s' -> Color: R=%.2f G=%.2f B=%.2f"),
-			i + 1, *LineText, TextColor.R, TextColor.G, TextColor.B);
-
+		// Markdown syntax highlighting while keeping the user's font
+		if (LineText->StartsWith("#"))
+		{
+			TextColor = highlightedColor;
+		}
+		else if (LineText->StartsWith("**") && LineText->EndsWith("**"))
+		{
+			TextColor = highlightedColor;
+		}
+		else if (LineText->StartsWith("_") && LineText->EndsWith("_"))
+		{
+			TextColor = highlightedColor;
+		}
+		else if (LineText->Contains("[") && LineText->Contains("]("))
+		{
+			TextColor = highlightedColor;
+		}
+		
 		FTextBlockStyle TextStyle;
-		TextStyle.SetFont(DefaultFont);
+		TextStyle.SetFont(CustomFont);
 		TextStyle.SetColorAndOpacity(TextColor);
 
-		TSharedRef<FSlateTextRun> TextRun = FSlateTextRun::Create(FRunInfo(), SharedLineText, TextStyle);
-		Runs.Add(TextRun);
-
-		LinesToAdd.Emplace(SharedLineText, MoveTemp(Runs));
+		Runs.Add(FSlateTextRun::Create(FRunInfo(), LineText, TextStyle));
+		LinesToAdd.Emplace(MoveTemp(LineText), MoveTemp(Runs));
 	}
-
-	UE_LOG(LogTemp, Warning, TEXT("Finished processing %d lines"), LinesToAdd.Num());
 
 	TargetTextLayout.AddLines(LinesToAdd);
 }
